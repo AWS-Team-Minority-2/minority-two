b64b9f77efb9462d8bd3f347232004a0d521272b
Deontae Smith <98364400+deontae-smith@users.noreply.github.com>
2024-01-23 20:47:50 -0500

    add routing

--- frontend/App.js
+++ frontend/App.js
@@ -1,10 +1,19 @@
 import { Homescreen } from './screens';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Homescreen />
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name='Home' component={Homescreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
