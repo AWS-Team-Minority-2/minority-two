418e4ec0280e6468416bc81a0deedbd85a9e934d
Deontae Smith <98364400+deontae-smith@users.noreply.github.com>
2024-02-20 00:09:43 -0500

    min 7.1

---
+++ apps/frontend/screens/Admin/login/screen.js
@@ -0,0 +1,59 @@
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import {
+  Image,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+  TextInput,
+} from 'react-native';
+// import { LoginForm } from './LoginForm';
+import { useScreenDispatch, changeScreen } from '@min-two/screen-iso';
+
+import styles from '../sass/Admin.scss';
+
+const AdminScreen = () => {
+  const navigation = useNavigation();
+  const dispatch = useScreenDispatch();
+
+  return (
+    <SafeAreaView style={styles.screenLayout}>
+      <View style={styles.screenAdjustment}>
+        <TouchableOpacity
+          style={styles.leftIcon}
+          onPress={() => {
+            changeScreen(dispatch, 'Landing');
+            navigation.navigate('Home');
+          }}
+        >
+          <Feather name='chevron-left' size={33} color='black' />
+        </TouchableOpacity>
+
+        <View style={styles.welcomeTextContainer}>
+          <Text style={styles.welcomeText}>Hello Admin!</Text>
+          <Text style={styles.subWelcomeText}>
+            Please enter your 5 dight code to continue
+          </Text>
+        </View>
+
+        <View style={styles.loginFieldsContainer}>
+          <View style={styles.fieldParent}>
+            <Text style={styles.fieldHeader}>Admin Code</Text>
+            <TextInput
+              placeholder='00000'
+              onChangeText={(newText) => handleFormChange('email', newText)}
+              style={styles.inputContainer}
+            />
+          </View>
+        </View>
+        <TouchableOpacity style={styles.loginBttn}>
+          <Text style={styles.loginText}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export { AdminScreen };
